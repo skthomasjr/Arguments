ab02784478ba6542642685966bbd86ae052de07b
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-14 00:30:20 -0600

    ~

--- Build/NuGet/Package.cmd
+++ Build/NuGet/Package.cmd
@@ -1,24 +1,17 @@
 @echo off
-echo Creating NuGet package
-set Name=%APPVEYOR_PROJECT_NAME%
-set Version=%APPVEYOR_BUILD_VERSION%
+echo Creating NuGet package...
+
 cd /d %~dp0
 
-echo.
-echo Creating convention based working directory...
 mkdir Working\build
 mkdir Working\content
 mkdir Working\lib
 mkdir Working\tools
 
-echo.
-echo Copying to lib...
-xcopy ..\..\Source\%Name%.Net35\bin\Release\* Working\lib\net35\* /s /e /y
-xcopy ..\..\Source\%Name%.Net40\bin\Release\* Working\lib\net40\* /s /e /y
-xcopy ..\..\Source\%Name%.Net45\bin\Release\* Working\lib\net45\* /s /e /y
-xcopy ..\..\Source\%Name%.Net46\bin\Release\* Working\lib\net46\* /s /e /y
-xcopy ..\..\Source\%Name%.Net461\bin\Release\* Working\lib\net461\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net35\bin\Release\* Working\lib\net35\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net40\bin\Release\* Working\lib\net40\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net45\bin\Release\* Working\lib\net45\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net46\bin\Release\* Working\lib\net46\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net461\bin\Release\* Working\lib\net461\* /s /e /y
 
-echo.
-echo Packaging...
-..\..\Tools\NuGet\nuget.exe pack Working\%NuGetPackageId%.nuspec -Version %Version%
+..\..\Tools\NuGet\nuget.exe pack Working\%NUGET_PACKAGE_ID%.nuspec -Version %APPVEYOR_BUILD_VERSION%
