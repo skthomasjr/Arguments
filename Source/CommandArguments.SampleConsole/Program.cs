122f1859f4a07881c10108cc1b32088e96ac6170
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-15 22:27:38 -0600

    NuGet package version fix.

--- Source/CommandArguments.SampleConsole/Program.cs
+++ Source/CommandArguments.SampleConsole/Program.cs
@@ -11,17 +11,17 @@ namespace CommandArguments.SampleConsole
 
             // Parameters: /a:"some value" //d --stop -w
 
-            Arguments.WithSource(args)
-                .AddArgument("a")
-                    .WithAction(parameter => { Console.WriteLine($"Parameter 'a' processed with value: {parameter}"); })
-                .AddArgument("d")
-                    .WithAction(parameter => { Console.WriteLine("Parameter 'd' processed"); })
-                .AddArgument("stop")
-                    .WithAction(parameter => { Console.WriteLine("Parameter 'stop' processed"); })
-                    .TerminateAfterExecution()
-                .AddArgument("w")
-                    .WithAction(parameter => { Console.WriteLine("Parameter 'w' processed"); })
-                .Process();
+            //Arguments.WithSource(args)
+            //    .AddArgument("a")
+            //        .WithAction(parameter => { Console.WriteLine($"Parameter 'a' processed with value: {parameter}"); })
+            //    .AddArgument("d")
+            //        .WithAction(parameter => { Console.WriteLine("Parameter 'd' processed"); })
+            //    .AddArgument("stop")
+            //        .WithAction(parameter => { Console.WriteLine("Parameter 'stop' processed"); })
+            //        .TerminateAfterExecution()
+            //    .AddArgument("w")
+            //        .WithAction(parameter => { Console.WriteLine("Parameter 'w' processed"); })
+            //    .Process();
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit.");
